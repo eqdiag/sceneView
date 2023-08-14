78eca204c19b2a649c891dfcdbef27995507f660
eqdiag <eqdiag@gmail.com>
2023-08-14 15:24:56 -0400

    first commit for scene view

--- src/core/settings.h
+++ src/core/settings.h
@@ -1,3 +1,3 @@
-#define ASSET_DIR  "C:/Users/Daniel/source/repos/gKernel/out/build/x64-Debug/../../../assets/" 
-#define SHADER_DIR  "C:/Users/Daniel/source/repos/gKernel/out/build/x64-Debug/../../../assets/shaders/"  
-#define MODEL_DIR  "C:/Users/Daniel/source/repos/gKernel/out/build/x64-Debug/../../../assets/models/" 
+#define ASSET_DIR  "C:/Users/Daniel/source/repos/sceneView/out/build/x64-Debug/../../../assets/" 
+#define SHADER_DIR  "C:/Users/Daniel/source/repos/sceneView/out/build/x64-Debug/../../../assets/shaders/"  
+#define MODEL_DIR  "C:/Users/Daniel/source/repos/sceneView/out/build/x64-Debug/../../../assets/models/" 
