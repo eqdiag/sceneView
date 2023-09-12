ed174483eeba6e7194dcfaf23adf04cf848ba14b
eqdiag <eqdiag@gmail.com>
2023-09-12 11:18:45 -0400

    added screenshots

--- src/core/settings.h
+++ src/core/settings.h
@@ -1,3 +1,3 @@
-#define ASSET_DIR  "/Users/eqdiag/code/sceneView/assets/" 
-#define SHADER_DIR  "/Users/eqdiag/code/sceneView/assets/shaders/"  
-#define MODEL_DIR  "/Users/eqdiag/code/sceneView/assets/models/" 
+#define ASSET_DIR  "C:/Users/Daniel/source/repos/sceneView/assets/" 
+#define SHADER_DIR  "C:/Users/Daniel/source/repos/sceneView/assets/shaders/"  
+#define MODEL_DIR  "C:/Users/Daniel/source/repos/sceneView/assets/models/" 
